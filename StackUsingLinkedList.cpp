a356c8b182ce1610996b78289adb2496b65f43a6
4D1FK4 <adifkapersonal0812@gmail.com>
2026-05-14 10:37:32 +0700

    case 1

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -92,4 +92,9 @@ while (choice != 5)
 
         switch (choice)
         {
+        case 1:
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value);
+            break;
     
