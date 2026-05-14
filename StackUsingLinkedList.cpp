4af328f7c723826c1a1cd7a6714f3f03ad49b17f
4D1FK4 <adifkapersonal0812@gmail.com>
2026-05-14 10:32:31 +0700

    else condition

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -54,3 +54,8 @@ public:
         {
             cout << "List is empty." << endl;
         }
+    else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
