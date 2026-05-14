766df723e5e5a20f6abefd5f87a9313ae7b2843c
4D1FK4 <adifkapersonal0812@gmail.com>
2026-05-14 10:33:15 +0700

    another else condition

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -59,3 +59,10 @@ public:
             Node *current = top;
             while (current != NULL)
             {
+{
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+    }
